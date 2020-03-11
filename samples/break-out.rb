9e4af050574263327307ab7834a34cb7f882a62c
stonesaw <ishichan1260@gmail.com>
2020-03-11 17:15:39 +0900

    Complete break-out

--- samples/break-out.rb
+++ samples/break-out.rb
@@ -1,10 +1,10 @@
 #ruby -Ku
-# break out - ブロック崩し
+# ブロック崩し
+# カーソルキーで移動
 require './../Monochrome-Ruby'
 
-map = Map.new(text_hash: {0 => "・"}, width: 32, height: 24)
-
-bar = Sprite.new(20, 22, "<---->")
+map = Map.new(text_hash: {0 => "  "}, width: 32, height: 24)
+bar = Sprite.new(20, 22, "<========>")
 
 walls = []
 map.height.times do |i|
@@ -20,19 +20,30 @@ dx = 1
 dy = -1
 
 blocks = []
-5.times do |y|
-  ((map.width / 2) - 2).times do |x|
-    blocks << Sprite.new(2 + x * 2 , 2 + y * 2, "■■")
+4.times do |y|
+  ((map.width - 4) / 4).times do |x|
+    blocks << Sprite.new(2 + x * 4 , 2 + y * 2, "('-ω-`)")
   end
 end
 
+flg = 0
+
 loop do
   Key.update
-
   break if Key.down?(Key::ESCAPE)
 
-  bar.x += 2 if Key.down?("2")
-  bar.x -= 2 if Key.down?("1")
+  if Key.down?(Key::RIGHT) && !bar.touch_right(walls)
+    bar.x += 3
+    while bar === walls
+      bar.x -= 1
+    end
+  end
+  if Key.down?(Key::LEFT) && !bar.touch_left(walls)
+    bar.x -= 3
+    while bar === walls
+      bar.x += 1
+    end
+  end
 
   ball.x += dx
   if (ball === walls || ball === bar)
@@ -46,6 +57,7 @@ loop do
     ball.x -= dx
     dx = -dx
   end
+  Sprite.clean(blocks)
   
   ball.y += dy
   if (ball === walls || ball === bar)
@@ -60,17 +72,23 @@ loop do
     dy = -dy
   end
   Sprite.clean(blocks)
-
+  
+  if blocks.length == 0
+    flg = 1
+    puts "============="
+    puts " GAME CLEAR!"
+    puts "============="
+  elsif ball.y >= map.height - 1
+    ball.text = "×"
+    flg = 1
+    puts "============"
+    puts " GAME OVER!"
+    puts "============"
+  end
+  
+  puts "block : #{blocks.length}"
   map.draw([bar, walls, ball, blocks])
 
-  if ball.y > map.height
-    puts "GAME OVER!"
-    break
-  end
-
-  p "ball x: #{ball.x}, y: #{ball.y}"
-  p "bar x: #{bar.x}, y: #{bar.y}, width: #{bar.width}"
-
-  sleep(0.1)
+  break if flg == 1
   system("cls")
 end
